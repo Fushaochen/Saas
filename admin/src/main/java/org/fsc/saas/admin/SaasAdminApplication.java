46665ac90546aaeee25d519b5262df17d2fff952
fushaochen <3460707218@qq.com>
2024-12-08 11:22:57 +0800

    项目初始化

---
+++ admin/src/main/java/org/fsc/saas/admin/SaasAdminApplication.java
@@ -0,0 +1,20 @@
+package org.fsc.saas.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ClassName:SaasAdminApplication
+ * Package:org.fsc.saas.admin
+ * Description:
+ *
+ * @Author: fsc
+ * @Create: 2024/12/8 - 11:20
+ * @Version: v1.0
+ */
+@SpringBootApplication
+public class SaasAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SaasAdminApplication.class,args);
+    }
+}
