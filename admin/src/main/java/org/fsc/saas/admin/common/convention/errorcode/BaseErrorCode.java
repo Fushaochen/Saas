122164ed281792058991750c7e697de96510194a
fushaochen <3460707218@qq.com>
2024-12-09 12:32:57 +0800

    统一全局返回实体对象

---
+++ admin/src/main/java/org/fsc/saas/admin/common/convention/errorcode/BaseErrorCode.java
@@ -0,0 +1,10 @@
+package org.fsc.saas.admin.common.convention.errorcode;/**
+ * ClassName:BaseErrorCode
+ * Package:org.fsc.saas.admin.common.convention.errorcode
+ * Description:
+ * @Author: fsc
+ * @Create: 2024/12/9 - 11:27
+ * @Version: v1.0
+ *   
+ */public enum BaseErrorCode {
+}
---
+++ admin/src/main/java/org/fsc/saas/admin/common/convention/errorcode/IErrorCode.java
@@ -0,0 +1,10 @@
+package org.fsc.saas.admin.common.convention.errorcode;/**
+ * ClassName:IErrorCode
+ * Package:org.fsc.saas.admin.common.convention.errorcode
+ * Description:
+ * @Author: fsc
+ * @Create: 2024/12/9 - 11:25
+ * @Version: v1.0
+ *   
+ */public interface IErrorCode {
+}
