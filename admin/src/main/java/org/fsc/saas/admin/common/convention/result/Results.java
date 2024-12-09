aed72067058b5120d630dccf41661e595793f8db
fushaochen <3460707218@qq.com>
2024-12-09 12:42:51 +0800

    业务全局异常码设计

---
+++ admin/src/main/java/org/fsc/saas/admin/common/convention/result/Results.java
@@ -0,0 +1,14 @@
+package org.fsc.saas.admin.common.convention.result;
+
+/**
+ * ClassName:Results
+ * Package:org.fsc.saas.admin.common.convention
+ * Description:
+ *
+ * @Author: fsc
+ * @Create: 2024/12/9 - 11:17
+ * @Version: v1.0
+ */
+public class Results {
+
+}
