9cefdc57a9ecc448f9c8dfd091170d1132928dc7
fushaochen <3460707218@qq.com>
2024-12-10 20:18:43 +0800

    查询用户名 布隆过滤器防止缓存穿透

---
+++ admin/src/main/java/org/fsc/saas/admin/config/RBloomFilterConfiguration.java
@@ -0,0 +1,26 @@
+package org.fsc.saas.admin.config;
+
+import org.redisson.api.RBloomFilter;
+import org.redisson.api.RedissonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * ClassName:RBloomFilterConfiguration
+ * Package:org.fsc.saas.admin.config
+ * Description:
+ *
+ * @Author: fsc
+ * @Create: 2024/12/10 - 20:11
+ * @Version: v1.0
+ */
+@Configuration
+public class RBloomFilterConfiguration {
+
+    @Bean
+    public RBloomFilter<String> userRegisterCachePenetrationBloomFilter(RedissonClient redissonClient){
+        RBloomFilter<String> cachePenetrationBloomFilter = redissonClient.getBloomFilter("userRegisterCachePenetrationBloomFilter");
+        cachePenetrationBloomFilter.tryInit(100000000L,0.001);
+        return cachePenetrationBloomFilter;
+    }
+}
--- admin/src/main/java/org/fsc/saas/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/org/fsc/saas/admin/service/impl/UserServiceImpl.java
@@ -3,14 +3,17 @@ package org.fsc.saas.admin.service.impl;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.fsc.saas.admin.common.convention.exception.ClientException;
 import org.fsc.saas.admin.common.enums.UserErrorCodeEnum;
 import org.fsc.saas.admin.dao.entity.UserDO;
 import org.fsc.saas.admin.dao.mapper.UserMapper;
 import org.fsc.saas.admin.dto.resp.UserRespDTO;
 import org.fsc.saas.admin.service.UserService;
+import org.redisson.api.RBloomFilter;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 /**
  * ClassName:UserServiceImpl
@@ -22,8 +25,10 @@ import org.springframework.stereotype.Service;
  * @Version: v1.0
  */
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements UserService {
 
+    private final RBloomFilter<String> userRegisterCachePenetrationBloomFilter;
 
     @Override
     public UserRespDTO getUserByUsername(String username) {
@@ -41,9 +46,6 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
 
     @Override
     public Boolean hasUsername(String username) {
-        LambdaQueryWrapper<UserDO> queryWrapper = Wrappers.lambdaQuery(UserDO.class)
-                .eq(UserDO::getUsername, username);
-        UserDO userDO = baseMapper.selectOne(queryWrapper);
-        return userDO != null;
+        return userRegisterCachePenetrationBloomFilter.contains(username);
     }
 }
