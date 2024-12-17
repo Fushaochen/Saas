8fad0b5dc0f6af5ef3d21636c34b746466d33289
fushaochen <3460707218@qq.com>
2024-12-17 17:05:47 +0800

    请求路径增加admin/

--- admin/src/main/java/org/fsc/saas/admin/controller/GroupController.java
+++ admin/src/main/java/org/fsc/saas/admin/controller/GroupController.java
@@ -27,30 +27,30 @@ public class GroupController {
 
     private final GroupService groupService;
 
-    @PostMapping("/api/saas/v1/group")
+    @PostMapping("/api/saas/admin/v1/group")
     public Result<Void> save(@RequestBody ShortLinkGroupSaveReqDTO requestParam){
         groupService.saveGroup(requestParam.getName());
         return Results.success();
     }
 
-    @GetMapping("/api/saas/v1/group")
+    @GetMapping("/api/saas/admin/v1/group")
     public Result<List<ShortLinkGroupRespDTO>> listGroup(){
         return Results.success(groupService.listGroup());
     }
 
-    @PutMapping("/api/saas/v1/group")
+    @PutMapping("/api/saas/admin/v1/group")
     public Result<Void> updateGroup(@RequestBody ShortLinkGroupUpdateReqDTO requestParam){
         groupService.updateGroup(requestParam);
         return Results.success();
     }
 
-    @DeleteMapping("/api/saas/v1/group")
+    @DeleteMapping("/api/saas/admin/v1/group")
     public Result<Void> deleteGroup(@RequestParam String gid){
         groupService.deleteGroup(gid);
         return Results.success();
     }
 
-    @PostMapping("/api/saas/v1/group/sort")
+    @PostMapping("/api/saas/admin/v1/group/sort")
     public Result<Void> sortGroup(@RequestBody List<ShortLinkGroupSortReqDTO> requestParam){
         groupService.sortGroup(requestParam);
         return Results.success();
--- admin/src/main/java/org/fsc/saas/admin/controller/UserController.java
+++ admin/src/main/java/org/fsc/saas/admin/controller/UserController.java
@@ -29,49 +29,49 @@ public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("/api/saas/v1/user/{username}")
+    @GetMapping("/api/saas/admin/v1/user/{username}")
     public Result<UserRespDTO> getUserByUsername(@PathVariable("username") String username){
         UserRespDTO result = userService.getUserByUsername(username);
         return Results.success(result);
     }
 
-    @GetMapping("/api/saas/v1/actual/user/{username}")
+    @GetMapping("/api/saas/admin/v1/actual/user/{username}")
     public Result<UserActualRespDTO> getActualUserByUsername(@PathVariable("username") String username){
         UserRespDTO result = userService.getUserByUsername(username);
         return Results.success(BeanUtil.toBean(result, UserActualRespDTO.class));
     }
-    @GetMapping("/api/saas/v1/user/has-username")
+    @GetMapping("/api/saas/admin/v1/user/has-username")
     public Result<Boolean> hasUsername(@RequestParam("username") String username){
         return Results.success(userService.hasUsername(username));
     }
 
-    @PostMapping("/api/saas/v1/user")
+    @PostMapping("/api/saas/admin/v1/user")
     public Result<Void> register(@RequestBody UserRegisterReqDTO requestParam){
         userService.register(requestParam);
         return Results.success();
     }
 
-    @PutMapping("/api/saas/v1/user")
+    @PutMapping("/api/saas/admin/v1/user")
     public Result<Void> update(@RequestBody UserUpdateReqDTO requestParam){
         userService.update(requestParam);
         return Results.success();
     }
 
 
-    @PostMapping("/api/saas/v1/user/login")
+    @PostMapping("/api/saas/admin/v1/user/login")
     public Result<UserLoginRespDTO> Login(@RequestBody UserLoginReqDTO requestParam){
         UserLoginRespDTO result = userService.login(requestParam);
         return Results.success(result);
     }
 
-    @GetMapping("/api/saas/v1/user/check-login")
+    @GetMapping("/api/saas/admin/v1/user/check-login")
     public Result<Boolean> checkLogin(@RequestParam("username") String username, @RequestParam("token") String token){
         Boolean result = userService.checkLogin(username, token);
         return Results.success(result);
     }
 
 
-    @DeleteMapping("/api/saas/v1/user/logout")
+    @DeleteMapping("/api/saas/admin/v1/user/logout")
     public Result<Void> logout(@RequestParam("username") String username,@RequestParam("token") String token){
         userService.logout(username, token);
         return Results.success();
