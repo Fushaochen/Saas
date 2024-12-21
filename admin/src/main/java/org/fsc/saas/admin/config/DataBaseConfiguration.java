0ae1481746e44cbc36e51c042c3024a571a01e1d
fushaochen <3460707218@qq.com>
2024-12-21 14:38:06 +0800

    提交MySQL数据库分页插件

---
+++ admin/src/main/java/org/fsc/saas/admin/config/DataBaseConfiguration.java
@@ -0,0 +1,27 @@
+package org.fsc.saas.admin.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * ClassName:DataBaseConfiguration
+ * Package:org.fsc.saas.project.config
+ * Description:
+ *
+ * @Author: fsc
+ * @Create: 2024/12/21 - 14:35
+ * @Version: v1.0
+ */
+@Configuration
+public class DataBaseConfiguration {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
